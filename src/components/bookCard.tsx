cbbd11f617af4b098f6097380ecb952b28532d81
uma <umamaheswari.g@everest.engineering>
2025-09-19 11:27:20 +0530

    Add book to cart functionality in BookCard

--- src/components/bookCard.tsx
+++ src/components/bookCard.tsx
@@ -1,20 +1,35 @@
 import React from "react";
-import { Book } from "../types/types";
+import { Book, CartItem } from "../types/types";
 import "./bookCard.css";
 
 type BookCardProps = {
   book: Book;
+  addToCart: (book: Book) => void;
+  removeFromCart: (bookId: string) => void;
+  cart: CartItem[];
 
 };
 
-const BookCard: React.FC<BookCardProps> = ({ book }) => {
+const BookCard: React.FC<BookCardProps> = ({ book, addToCart, removeFromCart, cart }) => {
+    const isAdded = cart.some(item => item.id === book.id);
+const handleButtonClick = () => {
+    if (isAdded) {
+      removeFromCart(book.id);
+    } else {
+      addToCart(book);
+    }
+  };
+  
   return (
     <div className="book-card">
       <img src={book.bookImage} alt={book.bookName} />
       <h3>{book.bookName}</h3>
       <p>{book.author}</p>
       <p>{book.price}</p>
-      <button className="cart-button">Add to cart</button>
+      <button onClick={handleButtonClick}
+     >
+        {isAdded ? 'Remove from cart' : 'Add To Cart'}
+      </button>
     </div>
   );
 };
