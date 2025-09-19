71a0952b4c2b6c5327db49ffed7a1c333f181939
uma <umamaheswari.g@everest.engineering>
2025-09-19 16:55:49 +0530

    Add increment button to the cart

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { CartItem } from "../types/types";
-import './cart.css'
+import "./cart.css";
 
 type CartProps = {
   cart: CartItem[];
-  removeFromCart?: (bookId: number) => void;
-};
+  increaseQty: (id: string) => void;
 
+};
 const Cart: React.FC<CartProps> = ({
   cart,
+  increaseQty,
   
 }) => {
+
   return (
     <div className="cart">
       <h2>Your Cart</h2>
+     
       {cart.length === 0 && <p>No items in cart</p>}
       {cart.map((item) => (
         <div key={item.id} className="cart-item">
@@ -21,14 +24,14 @@ const Cart: React.FC<CartProps> = ({
           <div>
             <h4>{item.bookName}</h4>
             <p>{item.author}</p>
-            <p>{item.price}</p>
+            <p>₹{item.price}</p>
+            <div className="cart-controls">
+              <button onClick={() => increaseQty(item.id)}>+</button>
+            </div>
           </div>
         </div>
       ))}
-     
     </div>
   );
 };
 export default Cart;
-
-
