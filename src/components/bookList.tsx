e407ffc1a41d4978e6274d930a1820fddc2a3e40
uma <umamaheswari.g@everest.engineering>
2025-09-19 11:28:08 +0530

    Add book to cart functionality in BookList

--- src/components/bookList.tsx
+++ src/components/bookList.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Book } from "../types/types";
+import { Book, CartItem } from "../types/types";
 import BookCard from "./bookCard";
 import "./bookList.css";
 
 type BookListProps = {
   books: Book[];
+  addToCart: (book: Book) => void;
+  removeFromCart: (bookId: string) => void;
+  cart: CartItem[];
 };
-const BookList: React.FC<BookListProps> = ({ books }) => {
+const BookList: React.FC<BookListProps> = ({ books, addToCart, removeFromCart, cart }) => {
   return (
     <div className="book-list">
       {books.map((book) => (
-        <BookCard key={book.id} book={book}  /> 
+        <BookCard key={book.id} book={book}  addToCart={addToCart} removeFromCart={removeFromCart} cart={cart} /> 
       ))}
     </div>
   );
