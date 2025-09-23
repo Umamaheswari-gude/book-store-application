94921f32e89e093f9b6b33459d9138fac11a52ff
uma <umamaheswari.g@everest.engineering>
2025-09-23 12:33:33 +0530

    Add the static data to the users

---
+++ src/components/data/userStore.tsx
@@ -0,0 +1,9 @@
+import { Users } from "../../types/types";
+
+export const users: Users[] = [
+  { email: "mahi@gmail.com", password: "mahi123" },
+  { email: "uma@gmail.com", password: "uma123" },
+  { email: "bhavi@gmail.com", password: "bhavi123" },
+  { email: "charvi@gmail.com", password: "charvi123" },
+];
+
