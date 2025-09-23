678f98d20ff17fa673c6f5c20c300cd6cb24994d
uma <umamaheswari.g@everest.engineering>
2025-09-24 02:35:33 +0530

    Refactor the application by context

--- src/bookStore.tsx
+++ src/bookStore.tsx
@@ -1,62 +1,38 @@
 import BookList from "./components/bookList";
 import "./bookStore.css";
 import { useBooks } from "./utils/api";
-import  { useState } from "react";
-import { Book, CartItem } from "./types/types";
-import Cart from "./components/cart";
 import Navbar from "./components/navbar";
-
-
+import Cart from "./components/cart";
+import { useCart } from "./context/cartContext"; 
+import React from "react";
 
 function Application() {
   const books = useBooks();
-  const [cart, setCart] = useState<CartItem[]>([]);
-  const [search, setSearch] = useState("");
-
-  const addToCart = (book: Book) => {
-    setCart((prev) => {
-    return [...prev, { ...book, quantity: 1 }];
-    });
-  };
-  
-  const removeFromCart = (bookId: string) => {
-    setCart((prev) => prev.filter((item) => item.id !== bookId));
-  };
-
-  const filteredBooks = books.filter((book) =>
-    book.bookName.toLowerCase().includes(search.toLowerCase()) ||
-    book.author.toLowerCase().includes(search.toLowerCase())
+  const { cart, addToCart, removeFromCart, increaseQty, decreaseQty } = useCart(); 
+  const [search, setSearch] = React.useState("");
+  const filteredBooks = books.filter(
+    (book) =>
+      book.bookName.toLowerCase().includes(search.toLowerCase()) ||
+      book.author.toLowerCase().includes(search.toLowerCase())
   );
-
-  const increaseQty = (id: string) => {
-    setCart((prev) =>
-      prev.map((item) =>
-        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
-      )
-    );
-  };
-  const decreaseQty = (id: string) => {
-    setCart(prev =>
-      prev.map(item =>
-        item.id === id
-          ? { ...item, quantity: item.quantity - 1 } : item ).filter(item => item.quantity > 0)
-      );
-  };
-
   return (
     <div className="head">
-     <Navbar search={search} setSearch={setSearch} />
+      <Navbar search={search} setSearch={setSearch} />
       <div className="main-content">
-        <BookList  books={filteredBooks} addToCart={addToCart} removeFromCart={removeFromCart} cart={cart} />
+        <BookList
+          books={filteredBooks}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+          cart={cart}
+        />
         <Cart
           cart={cart}
-          increaseQty={increaseQty}
-          decreaseQty={decreaseQty}
+          increaseQty={(id) => increaseQty(id)}
+          decreaseQty={(id) => decreaseQty(id)}
           removeFromCart={removeFromCart}
         />
       </div>
     </div>
   );
 }
-
 export default Application;
