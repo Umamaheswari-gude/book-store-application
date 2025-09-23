8fa37f1ef50dcc8fca20a4494b74eaeb6897c14e
uma <umamaheswari.g@everest.engineering>
2025-09-23 15:02:22 +0530

    Refactor the static data

--- src/components/data/userStore.tsx
+++ src/components/data/userStore.tsx
@@ -1,12 +1,8 @@
 import { Users } from "../../types/types";
 
-export const users: Users[] = [
-  {firstName: "Mahi", lastName: "Gude", email: "mahi@gmail.com", password: "mahi123" },
-  {firstName: "Uma", lastName: "Gude", email: "uma@gmail.com", password: "uma123" },
-  {firstName: "Bhavi", lastName: "Muppalla", email: "bhavi@gmail.com", password: "bhavi123" },
-  {firstName: "Charvi", lastName: "Muppalla", email: "charvi@gmail.com", password: "charvi123" },
+export const staticUsers: Users[] = [
+  { firstName: "Mahi", lastName: "Gude", email: "mahi@gmail.com", password: "mahi123" },
+  { firstName: "Uma", lastName: "Gude", email: "uma@gmail.com", password: "uma123" },
+  { firstName: "Bhavi", lastName: "Muppalla", email: "bhavi@gmail.com", password: "bhavi123" },
+  { firstName: "Charvi", lastName: "Muppalla", email: "charvi@gmail.com", password: "charvi123" },
 ];
-export const addUser = (user: Users) => {
-  users.push(user);
-  console.log("Updated users:", users);
-};
