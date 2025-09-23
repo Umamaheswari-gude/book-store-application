74892d0fa3af7c6ce1e513d5354e881fd089537f
uma <umamaheswari.g@everest.engineering>
2025-09-24 02:34:27 +0530

    Update app with routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,27 @@ import Application from './bookStore';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from './components/login';
 import Register from './components/register';
+import Wishlist from './components/wishlist';
+import { WishlistProvider } from './context/wishlistContext';
+import { CartProvider } from './context/cartContext';
 
 function App() {
   return (
+    <CartProvider>
+    <WishlistProvider>
       <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/register" replace />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/books" element={<Application />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/books" element={<Application />} />
+        <Route path="/wishlist" element={<Wishlist />} />
       </Routes>
     </Router> 
+    </WishlistProvider>
+    </CartProvider>
   );
 }
 
 export default App;
+
