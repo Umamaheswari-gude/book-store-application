2dc437e0511809ed7fbd19265e34d1cc8dd15b49
uma <umamaheswari.g@everest.engineering>
2025-09-23 13:45:22 +0530

    Add route path to register

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import './App.css';
 import Application from './bookStore';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from './components/login';
+import Register from './components/register';
 
 function App() {
   return (
       <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/register" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/books" element={<Application />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router> 
   );
