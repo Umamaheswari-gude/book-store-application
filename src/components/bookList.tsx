92ec493dc24177e78dfc58667ce78e413a9fa60c
uma <umamaheswari.g@everest.engineering>
2025-09-24 02:22:06 +0530

    Update the bookList component

--- src/components/bookList.tsx
+++ src/components/bookList.tsx
@@ -9,11 +9,12 @@ type BookListProps = {
   removeFromCart: (bookId: string) => void;
   cart: CartItem[];
 };
+
 const BookList: React.FC<BookListProps> = ({ books, addToCart, removeFromCart, cart }) => {
   return (
     <div className="book-list">
       {books.map((book) => (
-        <BookCard key={book.id} book={book}  addToCart={addToCart} removeFromCart={removeFromCart} cart={cart} /> 
+        <BookCard key={book.id} book={book}  addToCart={addToCart} removeFromCart={removeFromCart} cart={cart}  /> 
       ))}
     </div>
   );
