8ad9a33e9e61db6940fb4dc286fcc714545ffabe
uma <umamaheswari.g@everest.engineering>
2025-09-24 11:46:47 +0530

    Add the add to cart functionality using context

--- src/components/bookDetails.tsx
+++ src/components/bookDetails.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useBooks } from "../utils/api";
+import './bookDetails.css';
+import { useCart } from "../context/cartContext";
 
 const BookDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const books = useBooks();
+  const { addToCart, cart, removeFromCart } = useCart();
   const book = books.find((book) => book.id === id);
   if (!book) return <p>Book not found!</p>;
+  const inCart = cart.some((c) => c.id === book.id);
+
 
   return (
     <div className="book-details-container">
@@ -18,10 +23,20 @@ const BookDetails: React.FC = () => {
         <h4>{book.author}</h4>
         <p className="price">₹{book.price}</p>
         <h3>Description</h3>
-        <p>{book.description}</p>         
+        <p>{book.description}</p>  
+        <div className="actions">
+          <button
+            className={`cart-btn-details ${inCart ? "remove" : "add"}`}
+            onClick={() =>
+              inCart ? removeFromCart(book.id) : addToCart(book)}>
+            {inCart ? "Remove from Cart" : "Add to Cart"}
+          </button>       
           
         </div>
       </div>
+    </div>
   );
 };
 export default BookDetails;
+
+
