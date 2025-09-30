323ed7ca404e768b5c50bd6249188b3e31afb010
uma <umamaheswari.g@everest.engineering>
2025-09-30 13:51:07 +0530

    Add test case for adding a book to the wishlist

---
+++ src/tests/context/wishlistContext.test.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { renderHook, act } from "@testing-library/react";
+import { Book } from "../../types/types";
+import { useWishlist, WishlistProvider } from "../../context/wishlistContext";
+
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <WishlistProvider>
+    {children}
+  </WishlistProvider>
+);
+const sampleBook: Book = {
+  id: "1",
+  bookName: "400 Days",
+  author: "Chetan bhagat",
+  price: 200,
+  description: "Wishlist test book",
+  bookImage: "wishlist.jpg",
+};
+
+describe("WishlistContext", () => {
+  test("adds a book to wishlist", () => {
+    const { result } = renderHook(() => useWishlist(), { wrapper });
+    act(() => {
+      result.current.addToWishlist(sampleBook);
+    });
+    expect(result.current.wishlist).toHaveLength(1);
+    expect(result.current.wishlist[0].id).toBe("1");
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
