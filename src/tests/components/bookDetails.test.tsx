59a75e3db8ffb1c20292fee660720c38007d6f1d
uma <umamaheswari.g@everest.engineering>
2025-09-30 19:28:47 +0530

    Add test cases for bookDetails component

---
+++ src/tests/components/bookDetails.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import BookDetails from "../../components/bookDetails";
+
+beforeAll(() => {
+  jest.spyOn(console, 'warn').mockImplementation((...args) => {
+    if (typeof args[0] === 'string' && args[0].includes('React Router Future Flag Warning')) {
+      return;
+    }
+    console.warn(...args);
+  });
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
+
+jest.mock("../../context/cartContext", () => ({
+  useCart: () => ({
+    cart: [],
+    addToCart: jest.fn(),
+    removeFromCart: jest.fn(),
+  }),
+}));
+jest.mock("../../context/wishlistContext", () => ({
+  useWishlist: () => ({
+    wishlist: [],
+    addToWishlist: jest.fn(),
+    removeFromWishlist: jest.fn(),
+  }),
+}));
+jest.mock("../../utils/api", () => ({
+  useBooks: () => [
+    {
+      id: "1",
+      bookName: "The tale teller",
+      author: "Anne",
+      price: 500,
+      description: "Fifth novel in leaphorn",
+      bookImage: "test.png",
+    },
+  ],
+}));
+describe("BookDetails Component", () => {
+  
+  test("shows Add to Cart button when not in cart", () => {
+    render(
+      <MemoryRouter initialEntries={["/books/1"]}>
+        <Routes>
+          <Route path="/books/:id" element={<BookDetails />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    expect(screen.getByText("Add to Cart")).toBeInTheDocument();
+  });
+});
