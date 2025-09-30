f9fc3c48e0d0263a3d3cf7c218da5b8cb9271db6
uma <umamaheswari.g@everest.engineering>
2025-09-30 13:57:48 +0530

    Add test case to add books to cart

---
+++ src/tests/context/cartContext.test.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { renderHook, act } from "@testing-library/react";
+import { Book } from "../../types/types";
+import { CartProvider, useCart } from "../../context/cartContext";
+
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <CartProvider>
+    {children}
+  </CartProvider>
+);
+const sampleBook: Book = {
+  id: "1",
+  bookName: "Test Book",
+  author: "Author",
+  price: 100,
+  description: "A test book",
+  bookImage: "test.jpg",
+};
+describe("CartContext", () => {
+    
+  test("adds a book to the cart", () => {
+    const { result } = renderHook(() => useCart(), { wrapper });
+    act(() => {
+      result.current.addToCart(sampleBook);
+    });
+    expect(result.current.cart).toHaveLength(1);
+    expect(result.current.cart[0]).toMatchObject({ id: "1", quantity: 1 });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
