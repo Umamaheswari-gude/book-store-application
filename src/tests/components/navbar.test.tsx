df7bf3df5ab45534e0919515bbcc12ef45d9aa6b
uma <umamaheswari.g@everest.engineering>
2025-10-04 13:52:23 +0530

    Update test cases of navbar component

--- src/tests/components/navbar.test.tsx
+++ src/tests/components/navbar.test.tsx
@@ -2,27 +2,11 @@ import { render, screen } from "@testing-library/react";
 import Navbar from "../../components/navbar";
 
 describe("Navbar Component", () => {
-
-  test("input should auto-focus on mount", () => {
-    const mockSetSearch = jest.fn();
-    render(<Navbar search="" setSearch={mockSetSearch} />);
-    const input = screen.getByPlaceholderText(/Search for Books/i);
-    expect(input).toHaveFocus();
-  });
-
   test("input reflects controlled search value", () => {
     const mockSetSearch = jest.fn();
     render(<Navbar search="ReactJS" setSearch={mockSetSearch} />);
     const input = screen.getByPlaceholderText(/Search for Books/i) as HTMLInputElement;
     expect(input.value).toBe("ReactJS");
   });
+
 });
-
-
-
-
-
-
-
-
-
